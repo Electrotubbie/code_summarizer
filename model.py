21f8158acc985d507dc7242c4a5741f8c38fa7a7
Electrotubbie <steen2033@yandex.ru>
2024-05-27 00:17:23 +0500

    update model.py

--- model.py
+++ model.py
@@ -1,2 +1,12 @@
-def summarize(code):
-    return code
+from transformers import AutoTokenizer
+from transformers import AutoModelWithLMHead
+from transformers import SummarizationPipeline
+
+
+hug_repo = "SEBIS/code_trans_t5_base_code_documentation_generation_python"
+
+summarize = SummarizationPipeline(
+    model=AutoModelWithLMHead.from_pretrained(hug_repo),
+    tokenizer=AutoTokenizer.from_pretrained(hug_repo, skip_special_tokens=True),
+    # device=0
+)
