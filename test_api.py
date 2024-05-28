024c05405a70478699d01093b5ea718301d21bfc
VasiliiZema <zemovv@gmail.com>
2024-05-29 03:25:26 +0500

    Added test_2 post api

--- test_api.py
+++ test_api.py
@@ -1,16 +1,16 @@
 from fastapi.testclient import TestClient
 from api import app
+from api import summarize
 
 client = TestClient(app)
 
-def test_api():
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json()["description"] == ("API для генерации summary на python код. "
     "Возможности: простое summary методом summarize, по ссылке на файл в github, по python коду в репозитории.")
 
-# def test_summarize():
-#     response = client.post("/summarize")
-#     assert response == {'error': 'Пожалуйста, передайте в API '
-#                 '{\'code\': \'Ваш код\', \'gpt\': \'True/False\''}
+def test_summarize_raw_link():
+    response = client.post("/summarize/raw_url")
+    assert response.status_code == 200
 
